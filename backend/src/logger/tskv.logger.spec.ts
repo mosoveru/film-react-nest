bef52ddeb539e13e565747a8db0b266f8d81717f
Mosoveru <mosoveru@gmail.com>
2025-02-15 14:14:49 +0300

    feat: implement tskv logger spec
    Added tests for tskv logger and made some fixes in the TskvLogger for correct string formatting.

---
+++ backend/src/logger/tskv.logger.spec.ts
@@ -0,0 +1,81 @@
+import { TskvLogger } from './tskv.logger';
+
+describe('Тестирование TSKVLogger', () => {
+  let logger: TskvLogger;
+  const MESSAGE = 'Hello World!';
+  const ADDITIONAL_PARAM = 'Additional Param';
+  const ANOTHER_ADDITIONAL_PARAM = 'Another Param';
+
+  enum LEVELS {
+    LOG = 'log',
+    ERROR = 'error',
+    INFO = 'info',
+    WARN = 'warn',
+  }
+
+  const returnBlankStrWithParams = (level: string) =>
+    `level=${level}\tmessage=${MESSAGE}\toptionalParam-1=${ADDITIONAL_PARAM}\toptionalParam-2=${ANOTHER_ADDITIONAL_PARAM}\n`;
+  const returnBlankStrWithoutParams = (level: string) =>
+    `level=${level}\tmessage=${MESSAGE}\n`;
+
+  beforeEach(() => {
+    logger = new TskvLogger();
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('Проверка корректного форматирования строки', () => {
+    const receivedStrWithParams = logger.formatMessage(
+      LEVELS.INFO,
+      MESSAGE,
+      ADDITIONAL_PARAM,
+      ANOTHER_ADDITIONAL_PARAM,
+    );
+    const receivedStrWithoutParams = logger.formatMessage(LEVELS.INFO, MESSAGE);
+
+    expect(receivedStrWithParams).toEqual(
+      returnBlankStrWithParams(LEVELS.INFO),
+    );
+    expect(receivedStrWithoutParams).toEqual(
+      returnBlankStrWithoutParams(LEVELS.INFO),
+    );
+  });
+
+  it('Проверка работы метода log', () => {
+    const spy = jest.spyOn(console, 'log');
+
+    logger.log(MESSAGE, ADDITIONAL_PARAM, ANOTHER_ADDITIONAL_PARAM);
+
+    expect(spy).toHaveBeenCalledWith(returnBlankStrWithParams(LEVELS.LOG));
+
+    logger.log(MESSAGE);
+
+    expect(spy).toHaveBeenCalledWith(returnBlankStrWithoutParams(LEVELS.LOG));
+  });
+
+  it('Проверка работы метода warn', () => {
+    const spy = jest.spyOn(console, 'log');
+
+    logger.warn(MESSAGE, ADDITIONAL_PARAM, ANOTHER_ADDITIONAL_PARAM);
+
+    expect(spy).toHaveBeenCalledWith(returnBlankStrWithParams(LEVELS.WARN));
+
+    logger.warn(MESSAGE);
+
+    expect(spy).toHaveBeenCalledWith(returnBlankStrWithoutParams(LEVELS.WARN));
+  });
+
+  it('Проверка работы метода error', () => {
+    const spy = jest.spyOn(console, 'log');
+
+    logger.error(MESSAGE, ADDITIONAL_PARAM, ANOTHER_ADDITIONAL_PARAM);
+
+    expect(spy).toHaveBeenCalledWith(returnBlankStrWithParams(LEVELS.ERROR));
+
+    logger.error(MESSAGE);
+
+    expect(spy).toHaveBeenCalledWith(returnBlankStrWithoutParams(LEVELS.ERROR));
+  });
+});
--- backend/src/logger/tskv.logger.ts
+++ backend/src/logger/tskv.logger.ts
@@ -1,29 +1,30 @@
-import { LoggerService, Injectable } from '@nestjs/common';
+import { Injectable, LoggerService } from '@nestjs/common';
 
 @Injectable()
 export class TskvLogger implements LoggerService {
   formatMessage(level: string, message: any, ...optionalParams: any[]) {
     const levelStr = `level=${level}\t`;
-    const messageStr = `message=${message}\t`;
+    const messageStr = `message=${message}`;
     const optionalParamsStr = optionalParams
       .map((param, index) => {
-        return `optionalParam-${index + 1}=${param}\t`;
+        return `optionalParam-${index + 1}=${param}`;
       })
-      .join('')
-      .concat('\n');
+      .join('\t');
 
-    return levelStr.concat(messageStr, optionalParamsStr);
+    return optionalParamsStr
+      ? levelStr + messageStr + '\t' + optionalParamsStr + '\n'
+      : levelStr + messageStr + '\n';
   }
 
   log(message: any, ...optionalParams: any[]) {
-    console.log(this.formatMessage('log', message, optionalParams));
+    console.log(this.formatMessage('log', message, ...optionalParams));
   }
 
   error(message: any, ...optionalParams: any[]) {
-    console.log(this.formatMessage('error', message, optionalParams));
+    console.log(this.formatMessage('error', message, ...optionalParams));
   }
 
   warn(message: any, ...optionalParams: any[]) {
-    console.log(this.formatMessage('warn', message, optionalParams));
+    console.log(this.formatMessage('warn', message, ...optionalParams));
   }
 }
