31cb4799b329be60fa8b73036eae76f2e57554b4
Mosoveru <mosoveru@gmail.com>
2025-02-16 14:52:28 +0300

    feat: add order controller tests
    Added order controller unit tests.

--- backend/src/order/order.controller.spec.ts
+++ backend/src/order/order.controller.spec.ts
@@ -1,18 +1,69 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrderController } from './order.controller';
+import { OrderService } from './order.service';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { TicketDto } from './dto/ticket.dto';
 
-describe('OrderController', () => {
-  let controller: OrderController;
+describe('Тестирование OrderController', () => {
+  let orderController: OrderController;
+  let orderService: OrderService;
+
+  const ticketsMock = [
+    {
+      film: 'd290f1ee-6c54-4b01-90e6-d701748f0851',
+      session: '95ab4a20-9555-4a06-bfac-184b8c53fe70',
+      daytime: '2023-05-29T10:30:00.001Z',
+      time: '2023-05-29T10:30:00.001Z',
+      day: '2023-05-29T10:30:00.001Z',
+      row: 2,
+      seat: 5,
+      price: 350,
+    },
+    {
+      film: 'd290f1ee-6c54-4b01-90e6-d701748f0851',
+      session: '95ab4a20-9555-4a06-bfac-184b8c53fe70',
+      daytime: '2023-05-29T10:30:00.001Z',
+      time: '2023-05-29T10:30:00.001Z',
+      day: '2023-05-29T10:30:00.001Z',
+      row: 2,
+      seat: 5,
+      price: 350,
+    },
+  ] satisfies TicketDto[];
+
+  const createOrderDtoMock = {
+    email: 'test@test.com',
+    phone: '+79999999999',
+    tickets: ticketsMock,
+  } satisfies CreateOrderDto;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrderController],
-    }).compile();
+      providers: [OrderService],
+    })
+      .overrideProvider(OrderService)
+      .useValue({
+        makeOrder: jest.fn(() => ticketsMock),
+      })
+      .compile();
 
-    controller = module.get<OrderController>(OrderController);
+    orderController = module.get<OrderController>(OrderController);
+    orderService = module.get<OrderService>(OrderService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('Метод .makeOrder() контроллера должен вызвать метод сервиса.', () => {
+    orderController.makeOrder(createOrderDtoMock);
+
+    expect(orderService.makeOrder).toHaveBeenCalledWith(createOrderDtoMock);
+  });
+
+  it('Метод .makeOrder() контроллера должен вернуть корректное значение', async () => {
+    const receivedValue = await orderController.makeOrder(createOrderDtoMock);
+
+    expect(receivedValue).toEqual({
+      total: ticketsMock.length,
+      items: ticketsMock,
+    });
   });
 });
--- backend/src/order/order.service.spec.ts
+++ backend/src/order/order.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrderService } from './order.service';
+import { TOKENS } from '../constants';
 
 describe('OrderService', () => {
   let service: OrderService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrderService],
+      providers: [
+        OrderService,
+        {
+          provide: TOKENS.REPOSITORY,
+          useValue: jest.fn(),
+        },
+      ],
     }).compile();
 
     service = module.get<OrderService>(OrderService);
