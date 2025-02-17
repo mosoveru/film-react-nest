fd1532ffd4e047229b43bc97a6ddd0075c6d07b0
Mosoveru <mosoveru@gmail.com>
2025-02-17 10:34:39 +0300

    fix: stories types
    Fixed story types to get rid of type errors while frontend building.

--- frontend/src/components/Layout/Layout.stories.tsx
+++ frontend/src/components/Layout/Layout.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
 import { Layout } from './Layout';
 
 const meta = {
@@ -14,6 +13,9 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {},
+export const Empty: Story = {
+  args: {
+    isLocked: false,
+    children: null,
+  }
 };
